023e05739ddfaede33568c3b2d494546151444c0
Szeplaki <szeplaki.eva1@gmail.com>
2021-10-14 11:33:16 +0200

    Try AI

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -51,21 +51,21 @@ def get_move(board, player):
         b = 2
     return a,b
 
-# def get_random_position(lst):
-#     move = random.choice(lst)
-#     return move
+def get_random_position(lst):
+    move = random.choice(lst)
+    return move
 
-def get_ai_move(board, player):
-    all_moves = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)]
-    move_index = []
-    x, y = get_move(board, player)
-    if (x,y) in all_moves:
-        all_moves.remove((x,y))
-    for i in range(len(all_moves)+1):
-        move_index.append(i)
-    next_move_index = random.choice(move_index)
-    next_ai_move = all_moves[next_move_index]
-    return next_ai_move
+# def get_AI_move(board, player):
+#     all_moves = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)]
+#     move_index = []
+#     x, y = get_move(board, player)
+#     if (x,y) in all_moves:
+#         all_moves.remove((x,y))
+#     for i in range(len(all_moves)+1):
+#         move_index.append(i)
+#     next_move_index = random.choice(move_index)
+#     next_ai_move = all_moves[next_move_index]
+#     return next_ai_move
 
 
 def mark(board, player):
@@ -76,47 +76,49 @@ def mark(board, player):
         print("This field is taken!")
         print(f"{player}'s turn")
         get_move(board,player)
-# def new_list(move,lst):
-#     while len(lst) > 0:
-#         lst.pop(lst.index(move))
-#     return lst
 
 
-# def get_ai_move(board, player):
-#     """Returns the coordinates of a valid move for player on board."""
-#     lst = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
-#     move = get_random_position(lst)
-#     new_list(move,lst)
-#     a = 0
-#     b = 0
-#     if move == "A1":
-#         a = 0
-#         b = 0
-#     elif move == "A2":
-#         a = 0
-#         b = 1
-#     elif move == "A3":
-#         a = 0
-#         b = 2
-#     elif move == "B1":
-#         a = 1
-#         b = 0
-#     elif move == "B2":
-#         a = 1
-#         b = 1
-#     elif move == "B3":
-#         a = 1
-#         b = 2
-#     elif move == "C1":
-#         a = 2
-#         b = 0
-#     elif move == "C2":
-#         a = 2
-#         b = 1
-#     elif move == "C3":
-#         a = 2
-#         b = 2
-#     return a,b
+def new_list(move,lst):
+    while len(lst) > 0:
+        lst.pop(lst.index(move))
+    return lst
+
+
+def get_ai_move(board, player):
+    """Returns the coordinates of a valid move for player on board."""
+    lst = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+    move = get_random_position(lst)
+    new_list(move,lst)
+    a = 0
+    b = 0
+    if move == "A1":
+        a = 0
+        b = 0
+    elif move == "A2":
+        a = 0
+        b = 1
+    elif move == "A3":
+        a = 0
+        b = 2
+    elif move == "B1":
+        a = 1
+        b = 0
+    elif move == "B2":
+        a = 1
+        b = 1
+    elif move == "B3":
+        a = 1
+        b = 2
+    elif move == "C1":
+        a = 2
+        b = 0
+    elif move == "C2":
+        a = 2
+        b = 1
+    elif move == "C3":
+        a = 2
+        b = 2
+    return a,b
 
 
 def mark(board, player):
@@ -124,10 +126,10 @@ def mark(board, player):
     if board[x][y] == ".":
         board[x][y] = player
 
-# def marking_AI(board, player):
-#     x, y = get_ai_move(board, player)
-#     if board[x][y] == ".":
-#         board[x][y] = player
+def marking_AI(board, player):
+    x, y = get_ai_move(board, player)
+    if board[x][y] == ".":
+        board[x][y] = player
       
     
     
@@ -199,7 +201,7 @@ def print_board(board):
     C  {board[2][0]} | {board[2][1]} | {board[2][2]}\n""")
 
 
-def tictactoe_game(mode='HUMAN-HUMAN'):
+def tictactoe_game(mode=1):
     board = init_board()
     print_board(board)
     player = 'X'
@@ -220,22 +222,22 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
         print("It's a tie")
     #if has_won(board, player) and not is_full(board):
 
-# def tictactoe_game(mode=2):
-#     board = init_board()
-#     print_board(board)
-#     player = 'X'
-#     while not is_full(board) and not has_won(board, player):
-#         marking_AI(board, player)
-#         print_board(board)
-#         if has_won(board, player):
-#             print(f'{player} won the game!')
-#             break
-#         if player == 'X':
-#             player = 'O'
-#             time.sleep(1)
-#         elif player == 'O':
-#             player = 'X'
-#             time.sleep(1)
+def tictactoe_gameAI(mode):
+    board = init_board()
+    print_board(board)
+    player = 'X'
+    while not is_full(board) and not has_won(board, player):
+        marking_AI(board, player)
+        print_board(board)
+        if has_won(board, player):
+            print(f'{player} won the game!')
+            break
+        if player == 'X':
+            player = 'O'
+            time.sleep(1)
+        elif player == 'O':
+            player = 'X'
+            time.sleep(1)
 
     
     
@@ -275,7 +277,7 @@ def valid_input_mode(game_mode):
         if game_mode == '1':
             tictactoe_game(mode=1)
         elif game_mode == '2':
-            tictactoe_game(mode=2)
+            tictactoe_gameAI(mode=2)
         elif game_mode == '3':
             tictactoe_game('HUMAN-AI PRO')
         elif game_mode == '4':
@@ -283,15 +285,15 @@ def valid_input_mode(game_mode):
         elif game_mode == 'Q' or 'q':
             quit()
 
-def main():
-    main_menu()
-    print_board(init_board())
-    while True:
-        modvalaszto = add_game_mode()
-        if modvalaszto == 1:
-            get_move()
-        else:
-            get_ai_move()
+# def main():
+#     main_menu()
+#     print_board(init_board())
+#     while True:
+#         change_mode = add_game_mode()
+#         if change_mode == 1:
+#             get_move()
+#         else:
+#             get_ai_move()
         
 
 
